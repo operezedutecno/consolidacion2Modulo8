6b21f570259c02668f5afbb43d6ff4385df532f3
maumatus <86168992+maumatus@users.noreply.github.com>
2021-10-29 09:56:31 -0300

    actualizacion SCRIPT BD-Creacion tabla boletas

--- bd/script.sql
+++ bd/script.sql
@@ -4,8 +4,18 @@ create table clientes(
 	nombre varchar(30) not null,
 	correo varchar(30) not null,
 	primary key (id)
-)
+);
 
 insert into clientes (rut, nombre, correo) values ('1-9', 'Osman Perez', 'mail@mail.com');
 insert into clientes (rut, nombre, correo) values ('1-8', 'Pedro Pablo Belmar', 'mail@hotmail.com.com');
 insert into clientes (rut, nombre, correo) values ('1-7', 'Mauricio Matus', 'mail@gmail.com');
+
+
+CREATE TABLE boletas(
+	
+	id serial NOT NULL PRIMARY KEY,
+	id_cliente INT NOT NULL,
+	fecha DATE NOT NULL,
+	FOREIGN KEY (id_cliente) REFERENCES clientes (id)  
+	
+);
